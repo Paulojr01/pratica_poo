aa8e6625237034c2f186a0e8fc6d5f1014d47eb9
Paulojr01 <106793350+Paulojr01@users.noreply.github.com>
2022-08-28 23:31:48 -0300

    teste

--- teste/Aluno.java
+++ teste/Aluno.java
@@ -2,8 +2,20 @@ package teste;
 
 public class Aluno{
 
-    public static void main(String[] args) {
-        
+    private String nome;
+    public Aluno(String nome, String matricula, int notaAV1, int notaAV2, int notaAE, String curso, String periodo) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.notaAV1 = notaAV1;
+        this.notaAV2 = notaAV2;
+        this.notaAE = notaAE;
+        this.curso = curso;
+        this.periodo = periodo;
     }
-
+    private String matricula;
+    private int notaAV1;
+    private int notaAV2;
+    private int notaAE;
+    private String curso;
+    private String periodo;
 }
---
+++ teste/app.java
@@ -0,0 +1,8 @@
+package teste;
+
+public class app {
+    
+    public static void main(String[] args) {
+        
+    }
+}
