626e738e40c7e8db1fa16ccf0de2da155e924f15
Paulo Alves <106793350+Paulojr01@users.noreply.github.com>
2022-09-22 21:50:47 -0300

    Pratica03
    Pratica 03 feito

--- br_com_carnaval/Main.java
+++ br_com_carnaval/Main.java
@@ -2,9 +2,45 @@ package br_com_carnaval;
 import javax.swing.JOptionPane;
 
 public class Main{
-
+    static double valor;
     public static void main(String[] args) {
-        JOptionPane.showInputDialog("<1> ingresso Normal \n, <2>ingresso Vip\n <3> ingresso Camarote");
+
+       int opcao = Integer.parseInt(JOptionPane.showInputDialog("<1> ingresso Normal \n,<2>ingresso Vip\n <3> ingresso Camarote\n <4>sair"));
+       // integer.parseIntt convert int para String o joption vai dentro do paramentro do mesmo.
+
+       switch (opcao) {
+        case 1 :
+            valor = Double.parseDouble(JOptionPane.showInputDialog("valor do ingresso:" ));
+            Normal ingressoAdicional = new Normal(valor);
+            JOptionPane.showMessageDialog(null, ingressoNormal.imprimeIngresso , title, messageType);
+
+            break;
+        }
+
+        case 2 :{
+                valor = Double.parseDouble(JOptionPane.showInputDialog("valor do ingresso:" ));
+                Normal ingressoAdicional = new Normal(valor);
+                JOptionPane.showMessageDialog(null, ingressoNormal.imprimeIngresso , title, messageType);
+
+            }
+
+        case 3 : {
+            valor = Double.parseDouble(JOptionPane.showInputDialog("Valor do ingresso Camarote: ", ));
+
+            break;
+
+        }
+
+        case 4 :{
+
+
+
+        }
+   
+   
+   
     }
 
 }
+
+// imput retorna valores de string temos que converte de string para double int etc..
--- br_com_carnaval/Normal.java
+++ br_com_carnaval/Normal.java
@@ -1,10 +1,24 @@
 package br_com_carnaval;
 
 public class Normal extends Ingresso {
+    private double normal;
+
 
     public Normal(double valor_Ingresso) {
         super(valor_Ingresso);
         
     }
+
+    public double getNormal() {
+        return normal;
+    }
+    public void setNormal(double normal) {
+        this.normal = normal;
+    }
+
+    public String imprimeValorIngresso(){
+        
+    }
+
     
 }
